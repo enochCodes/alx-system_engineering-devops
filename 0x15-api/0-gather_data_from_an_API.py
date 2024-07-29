2cec87aa8271ccf5864f839b9bb911f73dc16e81
enochCodes <henoksamson57@gmail.com>
2024-07-29 20:57:00 +0300

    fix_this

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,38 +1,14 @@
 #!/usr/bin/python3
-
-"""
-test api user
-"""
+"""Returns to-do list information for a given employee ID."""
 import requests
 import sys
 
-if len(sys.argv) != 2:
-    print("Usage: python3 0-gather_data_from_an_API.py <employee_id>")
-    sys.exit(1)
+if __name__ == "__main__":
+    url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(url + "users/{}".format(sys.argv[1])).json()
+    todos = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
 
-employee_id = int(sys.argv[1])
-
-user_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}"
-todos_url = f"https://jsonplaceholder.typicode.com/todos?userId={employee_id}"
-
-user_response = requests.get(user_url)
-todos_response = requests.get(todos_url)
-
-if user_response.status_code != 200 or todos_response.status_code != 200:
-    print("Failed to retrieve data")
-    sys.exit(1)
-
-user_data = user_response.json()
-todos_data = todos_response.json()
-
-
-employee_name = user_data.get("name")
-total_tasks = len(todos_data)
-done_tasks = [task for task in todos_data if task.get("completed")]
-
-
-number_of_done_tasks = len(done_tasks)
-
-print(f"Employee {employee_name} id done with tasks ({number_of_done_tasks}):")
-for task in done_tasks:
-    print(f"     {task.get('title')}")
+    completed = [t.get("title") for t in todos if t.get("completed") is True]
+    print("Employee {} is done with tasks({}/{}):".format(
+        user.get("name"), len(completed), len(todos)))
+    [print("\t {}".format(c)) for c in completed]
