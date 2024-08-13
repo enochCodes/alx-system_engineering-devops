9087242e59c0de13aa2ea65a0e01ff5a6fdb04e1
enochCodes <henoksamson57@gmail.com>
2024-08-13 10:00:26 +0300

    feat(scripting-api): request for redit to get sub number

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """
+    Queries the Reddit API and returns
+    """
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {'user-Agent': 'Mozilla/5.0'}
+
+    try:
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        if response.status_code == 200:
+            data = response.json()
+            return data.get('data', {}).get('subscribers', 0)
+        else:
+            return 0
+    except requests.ReqestException:
+        return 0
